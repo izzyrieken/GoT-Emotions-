0f56cc8a9ec44dc6165189d1c85e1d8bd81c7ba5
Isabelle Rieken <isabelle.rieken@gmail.com>
2020-05-05 15:08:05 -0700

    final push

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var app = {
 	episodes : [],
   list : [],
-	anguish : [0, 3],
-
-	rivalry : [2, 70],
-
 
 	initialize: function() {
-		var romance = [1, 71];
+		var romance = [12, 24, 52, 29, 32, 59, 64, 43, 61, 67];
+		var anguish = [71, 72, 13, 4, 10, 28, 36, 56, 33, 48, 23, 15, 18, 68, 69, 22, 14, 37, 45 ];
+		var competitive = [25, 46, 39, 70, 38, 11, 34, 35, 53, 58, 62, 5, 9, 6];
+		var triumphant = [2, 3, 27, 54, 65, 30, 41, 44 , 51, 7, 8, 44, 21, 40, 66 ];
+		var sinister = [26, 47, 63, 19, 20, 55, 42, 31, 57, 60, 16, 17, 49, 50];
 
     console.log ('button int.');
     $('.listEpisodes').click(function (){
@@ -18,17 +18,48 @@ var app = {
 			console.log ('romance pushed');
 			app.makeHTML(romance);
 		});
+		$('.anguish').click(function (){
+			console.log ('romance pushed');
+			app.makeHTML(romance);
+		});
+		$('.competitive').click(function (){
+			console.log ('romance pushed');
+			app.makeHTML(romance);
+		});
+		$('.triumphant').click(function (){
+			console.log ('romance pushed');
+			app.makeHTML(romance);
+		});
+		$('.sinister').click(function (){
+			console.log ('romance pushed');
+			app.makeHTML(romance);
+		});
 	},
 
 	makeHTML: function(emotions) {
+
 		var theHTML = '';
 		//var allEpisodes = new Array();
 		var allEpisodes = [];
 		// call API and get all episode
 		console.log("Get GoT Data");
-		for (var i = 1; i < 9; i++){
-			for(var j= 1; j < 11; j++){
-				var myURL = 'https://api.themoviedb.org/3/tv/1399/season/' + i +'/episode/'+ j + '?api_key=a0c315784d22203a07f5dd6e18520cb3&language=en-US'
+		for (var i = 0; i < emotions.length; i++){
+			  var index = emotions[i];
+				console.log("index "+index);
+				// calculate season & episode
+				if (index < 60) {
+					season = Math.floor(index/10);
+					episode = index%10;
+				} else if (index < 67) {
+					season = 7;
+					episode = (index-60)%7;
+				} else {
+					season = 8;
+					episode = (index-67)%6;
+				}
+				console.log("season,episode= "+season+","+episode);
+				// call api to get title
+				var myURL = 'https://api.themoviedb.org/3/tv/1399/season/' + season +'/episode/'+ episode + '?api_key=a0c315784d22203a07f5dd6e18520cb3&language=en-US'
 				// var allEpisodes = new Array(10);
 				console.log(myURL);
 				$.ajax({
@@ -41,41 +72,45 @@ var app = {
 					},
 					success: function(data){
 						console.log(data);
-						app.episodes = data.name;
+						//app.episodes = data.name;
 						// app.episodes = data.response.docs;
 						// console.log(app.episodes);
 						// app.makeHTML();
 						//allEpisodes.push ({name:data.name,season:data.season_number,episode:data.episode_number});
-						allEpisodes.push (data.name);
+						//allEpisodes.push (data.name);
+						// theHTML += "<div class='list' id='elementID'>";
+			      // theHTML += "";
+						// theHTML += "</div>";
+						// $('.container1').html(theHTML);
+
+						theHTML += "<div class='list' >";
+			      theHTML += "<h3>" + data.name + "</h3>";
+						theHTML += "</div>";
+						// document.getElementById(container1).innerHTML = "";
+						$('.container1').html(theHTML);
+
+
+
 					}
 				});
-			}
-		}
-		// list out episodes from selected emotion
-		var myStringArray = ["Hello","World"];
-		console.log(myStringArray);
-		var arrayLength = myStringArray.length;
-		for (var i = 0; i < arrayLength; i++) {
-		    console.log(myStringArray[i]);
-		    //Do something
 		}
 
-		console.log(emotions);
-		for (var i = 0; i < emotions.length; i++){
-			theHTML += "<div class='list'>";
-			console.log(emotions[i]);
-			var index = emotions[i];
-			var data = allEpisodes[0];
-			console.log("index here: " + index);
-			console.log(allEpisodes);
-			console.log("HERE: " + allEpisodes[2]);
-      theHTML += "<h3>" + data + "</h3>";
-			// theHTML += "<h3>" + app.episodes + "</h3>";
-			// theHTML += "<h3>" + number + "</h3>";
-			theHTML += "</div>";
-			// document.getElementById("list").innerHTML = number;
-		}
-		$('.container1').html(theHTML);
+		// console.log(emotions);
+		// for (var i = 0; i < emotions.length; i++){
+		// 	theHTML += "<div class='list'>";
+		// 	console.log(emotions[i]);
+		// 	var index = emotions[i];
+		// 	var data = allEpisodes[0];
+		// 	console.log("index here: " + index);
+		// 	console.log(allEpisodes);
+		// 	console.log("HERE: " + allEpisodes[2]);
+    //   theHTML += "<h3>" + data + "</h3>";
+		// 	// theHTML += "<h3>" + app.episodes + "</h3>";
+		// 	// theHTML += "<h3>" + number + "</h3>";
+		// 	theHTML += "</div>";
+		// 	// document.getElementById("list").innerHTML = number;
+		// }
+
 	},
 
 	getEpisodes: function() {
